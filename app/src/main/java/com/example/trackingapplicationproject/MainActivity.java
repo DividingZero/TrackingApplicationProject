00bd5a42ca0d5114516b86fe066a357f17199176
tommitah <reijotahvanainen15@gmail.com>
2019-03-06 15:57:17 +0200

    Notifications button working. Added openNotifications().

--- app/src/main/java/com/example/trackingapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/trackingapplicationproject/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.trackingapplicationproject;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -14,6 +17,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private Button notifications_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView date = (TextView)findViewById(R.id.currentDate);
         setDate(date);
+
+        notifications_button = (Button) findViewById(R.id.button8);
+        notifications_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNotifications();
+            }
+        });
     }
     public void setDate (TextView view){
 
@@ -30,5 +42,9 @@ public class MainActivity extends AppCompatActivity {
         view.setText(date);
     }
 
+    public void openNotifications() {
+        Intent intent = new Intent(this, notifications.class);
+        startActivity(intent);
+    }
 }
 
