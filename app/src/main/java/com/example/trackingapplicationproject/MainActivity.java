e4e704537a3fd63085eeca85c12d3a957cdb2076
DividingZero <sileoniluis@gmail.com>
2019-03-06 14:52:42 +0200

    Updated setDate method.

--- app/src/main/java/com/example/trackingapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/trackingapplicationproject/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.trackingapplicationproject;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView date = (TextView)findViewById(R.id.currentDate);
+        setDate(date);
     }
+    public void setDate (TextView view){
+
+        Date today = Calendar.getInstance().getTime();//getting date
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");//formatting the date
+        String date = formatter.format(today);
+        view.setText(date);
+    }
+
 }
+
